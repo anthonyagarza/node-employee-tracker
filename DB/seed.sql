093d8c8b92a4d028d8345574ac70ffcf42ddeec3
Anthony Garza <anthony.garza@pruvan.com>
2020-01-31 13:00:24 -0600

    add seed data to seed.sql

--- DB/seed.sql
+++ DB/seed.sql
@@ -33,3 +33,18 @@ CREATE TABLE employee (
         ON UPDATE NO ACTION
         ON DELETE SET NULL
 );
+
+INSERT INTO department (id, name) VALUES (1, 'Human Resources'), (2, 'Engineering'), (3, 'Legal');
+INSERT INTO role (id, title, salary, department_id) VALUES 
+	(1, 'Software Developer', 75000.00, 2), 
+    (2, 'Senior Software Developer', 95000.00, 2),
+    (3, 'Hiring Personnel', 55000.00, 1),
+    (4, 'Entry Liability Counsel', 90000.00, 3),
+    (5, 'General Counsel', 150000.00, 3);
+    
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
+	(1, 'Anthony', 'Garza', 2, NULL),
+    (2, 'Crystal', 'Ly', 1, 1), 
+    (3, 'Anathema', 'Smith', 5, NULL),
+    (4, 'Kim', 'Ruiz', 3, NULL),
+    (5, 'Jeff', 'Blaze', 4, 3);
